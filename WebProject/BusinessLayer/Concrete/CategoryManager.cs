3d66d6c7b0041970b478a8ffeb2aaaf3975da51c
Escan-DNMZ <84273839+Escan-DNMZ@users.noreply.github.com>
2021-12-31 11:22:01 +0300

    .

---
+++ WebProject/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager :  ICategoryService
+    {
+        EfCategoryRepository efCategoryRepository;
+
+        public CategoryManager()
+        {
+            efCategoryRepository = new EfCategoryRepository();
+        }
+        public void AddCategory(Category category)
+        {
+            efCategoryRepository.Insert(category);
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            efCategoryRepository.Delete(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return efCategoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return efCategoryRepository.GetListAll();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            efCategoryRepository.Update(category);
+        }
+    }
+}
